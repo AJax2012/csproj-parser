c66fb73f8bf9d929565549252a995a70f5db9461
Adam Gardner <adam.gardner@availity.com>
2022-09-12 12:37:43 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+const glob = require("glob");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const dirName = "";
+
+glob(`${dirName}/**/*.csproj`, null, function (err, files) {
+  if (err) throw err;
+
+  const assemblies = [];
+
+  files.forEach(file => {
+    fs.readFile(file, function (err, data) {
+      if (err) throw err;
+
+      const dom = new JSDOM(data);
+      const assemblyName = dom.window.document.getElementsByTagName("AssemblyName")[0].childNodes[0].nodeValue;
+      const outputType = dom.window.document.getElementsByTagName("OutputType")[0].childNodes[0].nodeValue;
+      
+      switch(outputType) {
+        case "Library": 
+          const name = `${assemblyName}.dll`;
+          if (!assemblies.includes(name) && !name.startsWith("Tester")) {
+            console.log(name);
+            assemblies.push(name);
+          }
+          break;
+        case "WinExe":
+        case "Exe":
+          const exeName = `${assemblyName}.exe`;
+          if (!assemblies.includes(exeName) && !exeName.startsWith("Tester")) {
+            console.log(exeName);
+            assemblies.push(exeName);
+          }
+          break;
+        default:
+          console.log(`Assembly: ${assemblyName}, OutputType: ${outputType}`);
+          break;
+      }
+    })
+  })
+})
